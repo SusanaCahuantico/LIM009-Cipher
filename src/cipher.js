4502d5a05ebcaf8261efd6729d8a133a603906e8
susanita12352 <scahuantico@gmail.com>
2019-03-11 09:23:07 -0500

    proyecto final

--- src/cipher.js
+++ src/cipher.js
@@ -1,68 +1,49 @@
 window.cipher = {
-  encode: (offset,string) => {
-    /* Acá va tu código */
-   // codecipher -> codigo ASCII de la letra cifrada
-   // position -> posicion de la letra cifrada en el alfabeto
-   let codeCipher, position;
-   //cadena final
-   let textResult = '';
-   //Recorrer cada uno de los caracteres del string
-   for (let i=0; i < string.length; i++) {
-     // Si el string es un espacio, se deja igual. No se aplica la formula
-     if(string[i] === ' '){
-       textResult += ' ';
-     }else{
-       //Obtener el codigo ASCII de la letra
-       const codeAscii = string.charCodeAt(i);
-       
-       if(codeAscii > 90){
-         position = codeAscii - 97 + (offset % 26);
-         //Formula para obtener el codigo ASCII de la letra CiFRADA
-         codeCipher = (position % 26) + 97;
-         
-         }
-         else{
-           position = codeAscii - 65 + (offset % 26);
-           codeCipher = (position % 26) + 65;
-         }
-         // Obtener la letra cifrada que corresponde 
-         const letterCipher = String.fromCharCode(codeCipher);
-         // Agregas la letra cifrada al resuktad
-         textResult += letterCipher;
-         }
-   }
-   return textResult;
-  },
-  decode: (offset, string) => {
-    let codeCipher, position;
-    let textResult = '';
-    for (let i = 0; i < string.length; i++){
-    if(string[i] === '') {
-      textResult += '';
-    } else{
-const codeAscii = string.charCodeAt(i);
-if(codeAscii > 90){
-position = codeAscii - 97 - (offset % 26);
-if(position < 0) {
-codeCipher = (position % 26) + 123;
-} else {
-  codeCipher = (position % 26) + 97;
-}
-} else {
-  position = codeAscii - 65 - (offset % 26);
-  if(position < 0){
-    codeCipher = (position % 26) + 91;
-  } else {
-    codeCipher = (position % 26) + 65;
-  }
-}
-const letterCipher = string.fromCharCode(codeCipher);
-textResult += letterCipher;
-}
-    }
-    return textResult;
-  },
-  createCipherWithOffset:(offset, string) => {
+	encode: (offset, string) => {
+		const despl = parseInt(offset);
+		let result = "";
 
-  }
+		for(let i=0; i<string.length; i++){
+			let number = string.charCodeAt(i);
+			if(number>=65 && number<=90){
+				result += String.fromCharCode((number - 65 + despl ) % 26 + 65);
+			}
+			else if (number === 32) {
+				 result += String.fromCharCode(32);
+			}
+			else if (number >= 97 && number <= 122) {
+				result += String.fromCharCode((number-97 + despl) % 26 + 97);
+			}
+			else if (number >= 33 && number <= 64) {
+				result += String.fromCharCode((number - 33 + despl) % 32 + 33);
+			}
+			else {
+				result = "Fuera de rango";
+			}
+		}  return result;
+	},
+
+	decode: (offset, string) => {
+		const despl = parseInt(offset);
+		let result = "";
+
+		for(let i=0; i<string.length; i++){
+			let number = string.charCodeAt(i);
+			if(number>=65 && number<=90){
+				result += String.fromCharCode((number - 65 - despl) % 26 + 65);
+			}
+			else if (number === 32) {
+				 result += String.fromCharCode(32);
+			}
+			else if (number >= 97 && number <= 122) {
+				result += String.fromCharCode((number - 97 - despl) % 26 + 97);
+			}
+			else if (number >= 33 && number <= 64) {
+				result += String.fromCharCode((number - 33 - despl) % 32 + 33);
+			}
+			else{
+				result = "Fuera de rango";
+			}
+		}  return result;
+	},
 };
