d850866cb7b7be8ac41a0e0b9c1c8d9b5aaf5d55
susanita12352 <scahuantico@gmail.com>
2019-03-11 01:57:14 -0500

    rehaciendo mi proyecto

--- src/index.js
+++ src/index.js
@@ -1,13 +1,24 @@
-const getData = (ChooseFunction) => {
-    const offset = parseInt(document.getElementById('number').value);
-if(chooseFunction){
-    const string = document.getElementById('text').value;
-    document.getElementById('text').value= '';
-    document.getElementById('text-result').value = cipher.encode(offset, string);
-} else {
-    const string = documente.getElementById('text-result').value;
-    document.getElementById('text-result').value = '';
-    document.getElementById('text').value = cipher.decode(offset, string);
-}
-}
-/* Acá va tu código */
+const offset = document.getElementById("offset");
+const mensaje = document.getElementById("mensaje");
+const btncifrar = document.getElementById("cifrar");
+const btndescifrar = document.getElementById("descifrar");
+const resulta = document.getElementById("result");
+const clear = document.getElementById("limpiar");
+
+btncifrar.addEventListener("click", () => {
+    const mensajeCapturado = mensaje.value;
+    const offsetCapturado = offset.value;
+    resulta.innerHTML = cipher.encode(offsetCapturado, mensajeCapturado);
+});
+
+btndescifrar.addEventListener("click", () => {
+    const mensajeCapturado = mensaje.value;
+    const offsetCapturado = offset.value;
+    resulta.innerHTML = cipher.decode(offsetCapturado, mensajeCapturado);
+});
+
+clear.addEventListener("click", () => {
+    mensaje.value = " ";
+    offset.value = " ";
+    resulta.innerHTML = " ";
+});
