f14438e938bf600a60b2fa6cab45f8d3b88441a2
susanita12352 <scahuantico@gmail.com>
2019-03-11 10:00:39 -0500

    proyecto cipher

--- src/index.js
+++ src/index.js
@@ -1,13 +1,49 @@
-const getData = (ChooseFunction) => {
-    const offset = parseInt(document.getElementById('number').value);
-if(chooseFunction){
-    const string = document.getElementById('text').value;
-    document.getElementById('text').value= '';
-    document.getElementById('text-result').value = cipher.encode(offset, string);
-} else {
-    const string = documente.getElementById('text-result').value;
-    document.getElementById('text-result').value = '';
-    document.getElementById('text').value = cipher.decode(offset, string);
-}
-}
-/* Acá va tu código */
+window.cipher = {
+	encode: (offset, string) => {
+		const despl = parseInt(offset);
+		let result = "";
+
+		for(let i=0; i<string.length; i++){
+			let number = string.charCodeAt(i);
+			if(number>=65 && number<=90){
+				result += String.fromCharCode((number - 65 + despl ) % 26 + 65);
+			}
+			else if (number === 32) {
+				 result += String.fromCharCode(32);
+			}
+			else if (number >= 97 && number <= 122) {
+				result += String.fromCharCode((number-97 + despl) % 26 + 97);
+			}
+			else if (number >= 33 && number <= 64) {
+				result += String.fromCharCode((number - 33 + despl) % 32 + 33);
+			}
+			else {
+				result = "Fuera de rango";
+			}
+		}  return result;
+	},
+
+	decode: (offset, string) => {
+		const despl = parseInt(offset);
+		let result = "";
+
+		for(let i=0; i<string.length; i++){
+			let number = string.charCodeAt(i);
+			if(number>=65 && number<=90){
+				result += String.fromCharCode((number - 65 - despl) % 26 + 65);
+			}
+			else if (number === 32) {
+				 result += String.fromCharCode(32);
+			}
+			else if (number >= 97 && number <= 122) {
+				result += String.fromCharCode((number - 97 - despl) % 26 + 97);
+			}
+			else if (number >= 33 && number <= 64) {
+				result += String.fromCharCode((number - 33 - despl) % 32 + 33);
+			}
+			else{
+				result = "Fuera de rango";
+			}
+		}  return result;
+	},
+};
